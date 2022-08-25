c1347ea622c5b9d11a711263c572e31c1b535429
karltayeb <karl.tayeb7@gmail.com>
2022-08-25 07:14:07 -0400

    rough

--- como/mococomo.py
+++ como/mococomo.py
@@ -1,7 +1,12 @@
+from operator import length_hint
 from typing import List
 
 from .component_distributions import ComponentDistribution
 from .logistic_regression import LogisticRegression
+from .utils import categorical_entropy_vec
+
+import jax.numpy as jnp
+import jax
 
 class MoreComponentCoMo:
     def __init__(self, data, f_list: List[ComponentDistribution], logreg_list: List[LogisticRegression]):
@@ -15,7 +20,7 @@ class MoreComponentCoMo:
         """
         self.data = data
         self.f_list = f_list
-        self.logreg_list = logreg_list
+        self.logreg_list = logreg_list  # TODO: pad with a Kth logistic regression that predicts 1.
         self.data['alpha'] = mococomo_compute_responsibilities(
             self.data, self.f_list, self.logreg_list
         )
@@ -106,22 +111,13 @@ class MoreComponentCoMo:
         """
         [f.update(self.data) for f in self.f_list];
 
-
-    def update_f1(self):
-        """
-        Update the alternate component
-        """
-        self.f1.update(self.data)
-
     def iter(self):
         """
         iteration updates responsibilities, logistic regression, f0, and f1
         """
         self.update_responsibilities()
-        self.logreg.update()
-        self.f0.update(self.data)
-        self.f1.update(self.data)
-
+        self.update_logreg()
+        self.update_f()
 
     def fit(self, niter=100, tol=1e-3):
         for i in range(niter):
@@ -134,3 +130,97 @@ class MoreComponentCoMo:
         return ((len(self.elbo_history) > 2)
             and ((self.elbo_history[-1] - self.elbo_history[-2]) < tol))
 
+
+###
+# Convert pi_tilde to pi-- prior mixture weights
+###
+def pi_scanner(gamma, pi_tilde):
+    """
+    produce the next pi from the current
+    cumulative probability and conditional probability
+    use this function to scan over conditional probabilities pi_tilde
+    """
+    pi = pi_tilde * (1. - gamma)
+    gamma = gamma + pi
+    return gamma, pi
+
+
+def pi_tilde2pi(pi_tilde):
+    """
+    convert vector of conditional mixture probabilities
+    to mixture probabilities pi
+
+    pi_tilde[k] = P(draw k given we did not draw 1... k-1)
+    pi[k] = P(draw k)
+    """
+    pi_tilde = jnp.concatenate([pi_tilde, jnp.array([1.0])])
+    _, pi = jax.lax.scan(pi_scanner, init=0., xs = pi_tilde)
+    return pi
+
+
+# vectorized version of pi_tilde2pi
+pi_tilde2pi_vec = jax.vmap(pi_tilde2pi, 0, 0)
+
+
+def mococomo_prior_mixture_weights(logreg_list: List[LogisticRegression]):
+    """
+    compute prior on mixture assignments from covariates
+    """
+    # K-1 x N with the last row all ones
+    pi_tilde = jnp.array([jax.nn.sigmoid(lr.predict()) for lr in logreg_list]).T
+    pi = pi_tilde2pi_vec(pi_tilde)
+    return pi
+
+
+###
+# Compute posterior assignment probabilities
+###
+
+def mococomo_compute_responsibilities(data: dict, f_list: List[ComponentDistribution], logreg_list: List[LogisticRegression]):
+    """
+    get posterior assignment probabilities
+    """
+    # N x K log likelihood of data under each component distribution
+    loglik = jnp.array([
+        f.convolved_logpdf(data['beta'], data['se']) for f in f_list]).T
+    logpi = jnp.log(mococomo_prior_mixture_weights(logreg_list))
+    post_pi = jax.nn.softmax(loglik + logpi, axis=1)
+    return post_pi
+
+
+def mococomo_loglik(data: dict, f_list: List[ComponentDistribution], pi: jnp.ndarray, sum: bool=True):
+    """
+    Compute the data log likelihood under mococomo model
+    Parameters:
+        data: a dictionary of data
+        f_list: a list of Component distribution objects K
+        pi: a N x K matrix of assignment probabilities
+        sum: Boolean, if True sum loglik across observations, 
+            if False return loglikelihood for each observations
+    """
+    loglik = jnp.array([
+        f.convolved_logpdf(data['beta'], data['se']) for f in f_list]).T
+
+    if sum:  # one number, data log likelihood
+        loglik = jnp.sum(loglik + pi)
+    else:  # log likelihood of each observation seperately
+        loglik = jnp.sum(loglik + pi, 1)
+    return loglik
+
+
+def mococomo_elbo(data, f_list, logreg_list, responsibilities):
+    data_loglik = mococomo_loglik(data, f_list, responsibilities, sum = False)
+    assignment_entropy = categorical_entropy_vec(responsibilities)
+
+    logreg_elbo = logreg.evidence()
+    total_elbo = jnp.sum(data_loglik + assignment_entropy) + logreg_elbo
+    return dict(
+        data_loglik=data_loglik,
+        assignment_entropy=assignment_entropy,
+        logistic_elbo=logreg_elbo,
+        total_elbo=total_elbo)
+
+
+def comp_Nk(responsiblities):
+    Nk = 1 - jnp.cumsum(responsiblities)
+    return Nk
